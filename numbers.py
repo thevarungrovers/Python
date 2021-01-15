370d601bbe9860f761ea9bdf56616da3288a5fa1
Varun Grover <grover82vinni@gmail.com>
2021-01-15 22:32:48 +0530

    numbers,strings

---
+++ numbers.py
@@ -0,0 +1,41 @@
+# import libraries
+import math
+import random
+
+# to get absolute value
+print(abs(-10))
+
+# get smallest value
+print(math.ceil(10.98))
+print(math.ceil(10.28))
+
+# get largest integer
+print(math.floor(10.98))
+print(math.floor(10.28))
+
+# log value
+print(math.log(15))
+
+# maximum value
+tup = [1,89,-4]
+print(max(tup))
+#min value
+print(min(tup))
+
+# get fractional and integer value
+print(math.modf(10.98))
+
+# return power of number
+print(math.pow(2,6))
+
+# round number
+print(round(10.56))
+print(round(10.568948,3))
+
+# squareroot
+print(math.sqrt(100)) 
+
+#value of pi
+print(math.pi)
+
+
---
+++ string.py
@@ -0,0 +1,20 @@
+a = "Hello World!"
+
+print(a.lower()) # lower characters
+
+print(a.upper()) # upper characters
+
+print(a.replace('!','!!')) # replace something
+
+print(a.split(' ')) # break the string to list
+
+print(a[2]) # print character at 2 index
+
+print(a[2:]) # print whole substring after index 2
+
+print(a[2:7]) # print substring between 2 and 7
+
+print(a[:5]) # print substring till index 5
+
+print(len(a)) # length of atring
+
